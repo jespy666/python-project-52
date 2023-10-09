0644089aa60ad4e9843a3a2640844448cadab5e2
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2023-10-09 20:49:44 +0600

    add permission mixin

---
+++ task_manager/mixins.py
@@ -0,0 +1,16 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
+
+
+class UserPermissionMixin(UserPassesTestMixin):
+
+    permission_message = None
+    permission_url = None
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.permission_message)
+        return redirect(self.permission_url)
