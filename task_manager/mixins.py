fedfd915b180698acc7c6b722e0c95d0baa1d1a8
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2023-10-16 17:59:30 +0600

    add permissions mixins

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.db.models import ProtectedError
 from django.shortcuts import redirect
 
 
@@ -22,4 +23,29 @@ class UserAuthRequiredMixin(LoginRequiredMixin):
         if not self.request.user.is_authenticated:
             messages.error(self.request, self.permission_denied_message)
             return redirect(self.login_url)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ObjectDeleteProtectionMixin:
+
+    protection_message = None
+    protected_url = None
+
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            messages.error(request, self.protection_message)
+            return redirect(self.protected_url)
+
+
+class TaskAuthorPermissionMixin:
+
+    author_url = None
+    author_message = None
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.id != self.get_object().author.id:
+            messages.error(request, self.author_message)
+            return redirect(self.author_url)
         return super().dispatch(request, *args, **kwargs)
