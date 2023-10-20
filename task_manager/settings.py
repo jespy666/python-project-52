4f1c6e338af6891b1c4a8ea5914893d0ac8d6c06
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2023-10-20 16:15:34 +0600

    prohibit collect foreign static

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -159,6 +159,10 @@ STATICFILES_DIRS = [
     os.path.join(f'{BASE_DIR}/task_manager', 'static')
 ]
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
